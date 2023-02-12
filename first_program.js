472d4268e887ccff174f831c3266775c7eef74e7
Sheraz Iqbal <105271296+Sheraz-k@users.noreply.github.com>
2023-02-12 11:46:03 +0500

    Add files via upload

---
+++ first_program.js
@@ -0,0 +1,9 @@
+console.log("Happy Journey of coding with Teresol at FAST NUCES..!!");
+
+let myname = "Sheraz";
+//Keyword+variale_name = data
+//concatination
+//typecasting(to convert different datatypes at any stage from chaging int to string -  changing university to hospital)
+//Nan
+myname=myname+2
+console.log(myname + " Welcome and Happy Journey of coding with Teresol at FAST NUCES..!!")
