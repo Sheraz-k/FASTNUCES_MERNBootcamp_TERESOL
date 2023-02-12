df44012cc65bd37c5c96cc614f1a1e2be1a02c8d
Sheraz Iqbal <105271296+Sheraz-k@users.noreply.github.com>
2023-02-12 12:01:11 +0500

    designArchitectures_and_designPatterns
    Design Patterns , Design Architecture

---
+++ designArchitectures_and_designPatterns.js
@@ -0,0 +1,12 @@
+//Session
+//Design Patterns //Rules and Regulations, Architecture to solve common prolems we face while programming 
+//Singolton Pattern(Restrics to Create only one oject from a class)//Factory Model (To Create a single mega class to comine all the dependant classess and addind Dependancy Injections to aotumate the class and oject creation process)
+//Repositoey Factory // to get all the data and API Connections from single repo
+//sandox
+
+//Design Architecture //how to write code (3 layer architecure)
+//MVC  //ORM (Oject Relational Mapper - Help in MApping Oject Relations)
+
+//MVVM 
+
+//MVP (Model View Presenter)
